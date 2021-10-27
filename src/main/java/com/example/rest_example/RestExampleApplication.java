c14c35de7da9165c11ed2f423fc38ba1b72517ce
kapcap96 <kapcap96@yandex.ru>
2021-10-27 22:48:28 +0300

    init

---
+++ src/main/java/com/example/rest_example/RestExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.rest_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExampleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestExampleApplication.class, args);
+    }
+
+}
